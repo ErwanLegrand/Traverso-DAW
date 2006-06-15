2e0230e5f423af003b331250cbcdc91e0715eb5f
Remon Sijrier <remon@traverso-daw.org>
2006-06-15 12:17:42 +0000

    bump version number

--- src/config.h
+++ src/config.h
@@ -24,16 +24,16 @@
 #define PACKAGE_NAME "traverso"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "traverso 0.26.0"
+#define PACKAGE_STRING "traverso 0.30.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "traverso"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.26.0"
+#define PACKAGE_VERSION "0.30.0"
 
 /* traverso resources directory */
 #define RESOURCES_DIR "/usr/local/share/traverso/"
 
 /* Version number of package */
-#define VERSION "0.26.0"
+#define VERSION "0.30.0"
