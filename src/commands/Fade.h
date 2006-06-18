84302caae536d48f1ee995d9cd8e5711eca4f16c
Remon Sijrier <remon@traverso-daw.org>
2006-06-18 21:15:15 +0000

    make Fade direction sensitive

--- src/commands/Fade.h
+++ src/commands/Fade.h
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 
-$Id: Fade.h,v 1.1 2006/05/08 20:01:30 r_sijrier Exp $
+$Id: Fade.h,v 1.2 2006/06/18 21:15:15 r_sijrier Exp $
 */
 
 #ifndef FADE_H
@@ -31,7 +31,7 @@ class AudioClip;
 class Fade : public Command
 {
 public :
-        Fade(AudioClip* clip, Curve* curve);
+        Fade(AudioClip* clip, Curve* curve, int direction);
         ~Fade();
 
         int begin_hold();
@@ -44,6 +44,7 @@ public :
 
 private :
         int origX;
+        int m_direction;
         double origFade;
         double newFade;
 
