550977e8a3e37d0cffdfc180cb75c2fc9ad57b5a
Remon Sijrier <remon@traverso-daw.org>
2006-06-20 19:16:42 +0000

    A generalized Gain Command class which is gonna to be used by Song, Track and AudioClip!

---
+++ src/commands/Gain.cpp
@@ -0,0 +1,107 @@
+/*
+Copyright (C) 2005-2006 Remon Sijrier 
+
+This file is part of Traverso
+
+Traverso is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
+
+$Id: Gain.cpp,v 1.1 2006/06/20 19:16:36 r_sijrier Exp $
+*/
+
+#include "Gain.h"
+
+#include "ContextItem.h"
+#include "ContextPointer.h"
+
+// Always put me below _all_ includes, this is needed
+// in case we run with memory leak detection enabled!
+#include "Debugger.h"
+
+
+Gain::Gain(ContextItem* context, float gain)
+	: Command(context)
+{
+	gainObject = context;
+	
+	if (gain >= 0) {
+		newGain = gain;
+		QMetaObject::invokeMethod(gainObject, "get_gain",
+					Qt::DirectConnection,
+					Q_RETURN_ARG(float, origGain));
+	}
+}
+
+
+Gain::~Gain()
+{}
+
+int Gain::prepare_actions()
+{
+	return 1;
+}
+
+int Gain::begin_hold()
+{
+	if ( ! QMetaObject::invokeMethod(gainObject, "get_gain",
+					Qt::DirectConnection,
+					Q_RETURN_ARG(float, origGain)) ) {
+		PWARN("Gain::begin_hold QMetaObject::invokeMethod failed");
+		return 0;
+	}	
+	
+	
+	newGain = origGain;
+	origY = cpointer().y();
+	
+	return 1;
+}
+
+int Gain::finish_hold()
+{
+	return 1;
+}
+
+int Gain::do_action()
+{
+	PENTER;
+	if ( ! QMetaObject::invokeMethod(gainObject, "set_gain", Q_ARG(float, newGain))) {
+		PWARN("Gain::do_action QMetaObject::invokeMethod failed");
+		return 0;
+	}	
+	
+	return 1;
+}
+
+int Gain::undo_action()
+{
+	PENTER;
+	if ( ! QMetaObject::invokeMethod(gainObject, "set_gain", Q_ARG(float, origGain)) ) {
+		PWARN("Gain::undo_action QMetaObject::invokeMethod failed");
+		return 0;
+	}
+	
+	return 1;
+}
+
+
+int Gain::jog()
+{
+	int ofy = origY - cpointer().y();
+	newGain  = (0.003f *  ofy)  + origGain;
+	return QMetaObject::invokeMethod(gainObject, "set_gain", Q_ARG(float, newGain));
+}
+
+// eof
+
---
+++ src/commands/Gain.h
@@ -0,0 +1,52 @@
+/*
+    Copyright (C) 2005-2006 Remon Sijrier 
+ 
+    This file is part of Traverso
+ 
+    Traverso is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+ 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+ 
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
+ 
+    $Id: Gain.h,v 1.1 2006/06/20 19:16:42 r_sijrier Exp $
+*/
+
+#ifndef GAIN_H
+#define GAIN_H
+
+#include "Command.h"
+
+class ContextItem;
+
+class Gain : public Command
+{
+public :
+        Gain(ContextItem* context, float gain = -1.0);
+        ~Gain();
+
+        int begin_hold();
+        int finish_hold();
+        int prepare_actions();
+        int do_action();
+        int undo_action();
+
+        int jog();
+
+private :
+	ContextItem*	gainObject;
+        float 		origGain;
+        float 		newGain;
+        int 		origY;
+};
+
+#endif
+ 
