0711a9a3fb67a58b58f74e2102e02e62a1774bd2
Remon Sijrier <remon@traverso-daw.org>
2006-06-23 11:21:02 +0000

    delete helpWindow in destructor, it's not owned by Qt

--- src/traverso/Interface.cpp
+++ src/traverso/Interface.cpp
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 
-$Id: Interface.cpp,v 1.7 2006/06/19 08:39:17 r_sijrier Exp $
+$Id: Interface.cpp,v 1.8 2006/06/23 11:21:02 r_sijrier Exp $
 */
 
 #include "../config.h"
@@ -83,6 +83,8 @@ Interface::~Interface()
 	settings.setValue("fullScreen", isFullScreen());
 	settings.setValue("pos", pos());
 	settings.endGroup();
+	
+	delete helpWindow;
 }
 
 void Interface::create()
