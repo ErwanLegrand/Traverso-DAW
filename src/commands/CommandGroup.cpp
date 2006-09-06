576daaf79d80e14abf24dbe8bf3db1ec17217778
Remon Sijrier <remon@traverso-daw.org>
2006-09-06 00:02:54 +0000

    first version

---
+++ src/commands/CommandGroup.cpp
@@ -0,0 +1,65 @@
+/*
+Copyright (C) 2005-2006 Remon Sijrier 
+
+This file is part of Traverso
+
+Traverso is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
+
+$Id: CommandGroup.cpp,v 1.1 2006/09/06 00:02:54 r_sijrier Exp $
+*/
+
+#include "CommandGroup.h"
+
+// Always put me below _all_ includes, this is needed
+// in case we run with memory leak detection enabled!
+#include "Debugger.h"
+
+
+int CommandGroup::prepare_actions()
+{
+	foreach(Command* cmd, m_commands) {
+		cmd->prepare_actions();
+	}
+	
+	return 1;
+}
+
+int CommandGroup::do_action()
+{
+	PENTER;
+	
+	foreach(Command* cmd, m_commands) {
+		cmd->do_action();
+	}
+	
+	return 1;
+}
+
+int CommandGroup::undo_action()
+{
+	PENTER;
+	
+	foreach(Command* cmd, m_commands) {
+		cmd->undo_action();
+	}
+	
+	return 1;
+}
+
+// eof
+
+
+ 
+ 
