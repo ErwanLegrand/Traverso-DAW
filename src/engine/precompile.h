9cf429e2390dddd09af4ff96d151b3ee11d032de
Remon Sijrier <remon@traverso-daw.org>
2006-12-04 19:34:22 +0000

    seems to pay of to have a precompile header for engine as well

---
+++ src/engine/precompile.h
@@ -0,0 +1,11 @@
+#include <qlist.h>
+#include <qvariant.h>  // All moc genereated code has this include
+#include <qobject.h>
+#include <qstring.h>
+#include <qstringlist.h>
+
+#include <qapplication.h>
+#include <qtimer.h>
+
+#include <glib.h>
+#include <stdlib.h>
