9bf6230268f649f711bf366999cdc6a717548b2c
Remon Sijrier <remon@traverso-daw.org>
2007-01-24 21:35:26 +0000

    removed debug code

--- src/core/CurveNode.cpp
+++ src/core/CurveNode.cpp
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 
-$Id: CurveNode.cpp,v 1.5 2007/01/24 21:18:30 r_sijrier Exp $
+$Id: CurveNode.cpp,v 1.6 2007/01/24 21:35:26 r_sijrier Exp $
 */
 
 #include "CurveNode.h"
@@ -29,7 +29,6 @@ $Id: CurveNode.cpp,v 1.5 2007/01/24 21:18:30 r_sijrier Exp $
 
 void CurveNode::set_relative_when_and_value( double relwhen, double value )
 {
-	printf("CurveNod:: range %f\n", m_curve->get_range());
 	m_when = relwhen * m_curve->get_range();
 	m_value = value;
 }
