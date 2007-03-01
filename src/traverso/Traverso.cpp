aedda4118a16341b5e4b59fe4b1703e30360e90c
Remon Sijrier <remon@traverso-daw.org>
2007-03-01 16:08:41 +0000

    minor cleanup

--- src/traverso/Traverso.cpp
+++ src/traverso/Traverso.cpp
@@ -17,7 +17,6 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 
-$Id: Traverso.cpp,v 1.24 2007/02/05 17:12:02 r_sijrier Exp $
 */
 
 #include <signal.h>
@@ -91,6 +90,7 @@ void Traverso::shutdown( int signal )
 	
 	// Just in case the mouse was grabbed...
 	cpointer().release_mouse();
+	QApplication::sendPostedEvents();
 	
 	switch(signal) {
 		case SIGINT:
@@ -100,17 +100,9 @@ void Traverso::shutdown( int signal )
 			break;
 		case SIGSEGV:
 			printf("\nCatched the SIGSEGV signal!\n");
-			switch (QMessageBox::critical( iface, "Crash",
-						"The program made an invalid operation and crashed :-(\n"
-						"should I try to save your work?",
-						"Yes",
-						"No",
-						0, 1)) {
-				case 0 :
-					break;
-				case 1 :
-					break;
-			}
+			QMessageBox::critical( iface, "Crash",
+				"The program made an invalid operation and crashed :-(\n"
+				"Please, report this to us!");
 	}
 
 	printf("Stopped\n");
