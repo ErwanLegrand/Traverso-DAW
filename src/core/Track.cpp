ebba3d93bfe2888a1115a607a04fb06805fd4d3c
Remon Sijrier <remon@traverso-daw.org>
2007-03-05 12:34:05 +0000

    getting rid of the timestamp/modification line

--- src/core/Track.cpp
+++ src/core/Track.cpp
@@ -17,7 +17,6 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 
-$Id: Track.cpp,v 1.39 2007/03/02 13:36:15 r_sijrier Exp $
 */
 
 #include "Track.h"
