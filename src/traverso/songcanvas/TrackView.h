a85e2c33b86cc5cd1dbe36a5176b35f7d3d8716b
Remon Sijrier <remon@traverso-daw.org>
2007-03-29 11:20:24 +0000

    added translation entry for select bus

--- src/traverso/songcanvas/TrackView.h
+++ src/traverso/songcanvas/TrackView.h
@@ -35,6 +35,7 @@ class TrackView : public ViewItem
 	Q_OBJECT
 	Q_CLASSINFO("edit_properties", tr("Edit properties"))
 	Q_CLASSINFO("add_new_plugin", tr("Add new Plugin")) 
+	Q_CLASSINFO("select_bus", tr("Select Bus")) 
 
 public:
 	TrackView(SongView* sv, Track* track);
