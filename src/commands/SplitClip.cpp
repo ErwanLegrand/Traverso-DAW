d643407894427e97b3be9fea0f289a568c94c256
Remon Sijrier <remon@traverso-daw.org>
2007-04-25 21:23:40 +0000

    * implemented [ X ], let's see how it works out. I think I like it more then first setting work cursor and then splitting

--- src/commands/SplitClip.cpp
+++ src/commands/SplitClip.cpp
@@ -24,28 +24,52 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 #include <libtraversocore.h>
 #include <SongView.h>
 #include <AudioClipView.h>
+#include <QGraphicsLineItem>
+#include <ViewItem.h>
 
 // Always put me below _all_ includes, this is needed
 // in case we run with memory leak detection enabled!
 #include "Debugger.h"
+
+class SplitCursor : public ViewItem
+{
+	Q_OBJECT
+public:
+		
+	SplitCursor(ViewItem* parent)
+	: ViewItem(parent, 0) 
+	{
+		m_boundingRect = QRectF(0, 0, 1, parent->boundingRect().height());
+	}
+	void paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWidget*) 
+	{
+		painter->drawLine(0, 0, 0, (int)m_boundingRect.height());
+	}
+};
+
+#include "SplitClip.moc"
 
 SplitClip::SplitClip(AudioClipView* view)
 	: Command(view->get_clip(), tr("Split Clip"))
 {
 	m_clip = view->get_clip();
 	m_sv = view->get_songview();
+	m_cv = view;
 	m_track = m_clip->get_track();
 	leftClip = 0;
 	rightClip = 0;
+	m_splitPoint = 0;
 	Q_ASSERT(m_clip->get_song());
 }
 
 
 int SplitClip::prepare_actions()
 {
-	nframes_t splitPoint = cpointer().scene_x() * m_sv->scalefactor;
+	if (! m_splitPoint) {
+		m_splitPoint = cpointer().scene_x() * m_sv->scalefactor;
+	}
 
-	if (splitPoint <= m_clip->get_track_start_frame() || splitPoint >= m_clip->get_track_start_frame() + m_clip->get_length()) {
+	if (m_splitPoint <= m_clip->get_track_start_frame() || m_splitPoint >= m_clip->get_track_start_frame() + m_clip->get_length()) {
 		return -1;
 	}
 
@@ -54,11 +78,11 @@ int SplitClip::prepare_actions()
 	
 	leftClip->set_song(m_clip->get_song());
 	leftClip->set_track_start_frame( m_clip->get_track_start_frame() );
-	leftClip->set_right_edge(splitPoint);
+	leftClip->set_right_edge(m_splitPoint);
 	
 	rightClip->set_song(m_clip->get_song());
-	rightClip->set_left_edge(splitPoint);
-	rightClip->set_track_start_frame( splitPoint);
+	rightClip->set_left_edge(m_splitPoint);
+	rightClip->set_track_start_frame(m_splitPoint);
 
 	return 1;
 }
@@ -90,6 +114,58 @@ int SplitClip::undo_action()
 	
 	resources_manager()->remove_clip_from_database(leftClip->get_id());
 	resources_manager()->remove_clip_from_database(rightClip->get_id());
+	
+	return 1;
+}
+
+int SplitClip::begin_hold()
+{
+	m_sv->start_shuttle(true, true);
+	m_splitcursor = new SplitCursor(m_cv);
+	return 1;
+}
+
+int SplitClip::finish_hold()
+{
+	delete m_splitcursor;
+	m_cv->update();
+	m_sv->start_shuttle(false);
+	return 1;
+}
+
+void SplitClip::cancel_action()
+{
+	finish_hold();
+}
+
+void SplitClip::set_cursor_shape(int useX, int useY)
+{
+	Q_UNUSED(useX);
+	Q_UNUSED(useY);
+	
+	cpointer().get_viewport()->set_holdcursor(":/cursorHoldLr");
+}
+
+
+int SplitClip::jog()
+{
+	int x = cpointer().scene_x();
+
+	if (x < 0) {
+		x = 0;
+	}
+
+	m_splitPoint = x * m_sv->scalefactor;
+
+	if (m_clip->get_song()->is_snap_on()) {
+		SnapList* slist = m_clip->get_song()->get_snap_list();
+		m_splitPoint = slist->get_snap_value(m_splitPoint);
+	}
+	
+	m_splitcursor->setPos(m_splitPoint / m_sv->scalefactor, 0);
+	m_sv->update_shuttle_factor();
+	cpointer().get_viewport()->set_holdcursor_text(frame_to_smpte(m_splitPoint, m_clip->get_song()->get_rate()));
+	cpointer().get_viewport()->set_holdcursor_pos(QPoint(cpointer().scene_x() - 16, cpointer().scene_y() - 16));
 	
 	return 1;
 }
--- src/commands/SplitClip.h
+++ src/commands/SplitClip.h
@@ -17,7 +17,7 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
  
-    $Id: SplitClip.h,v 1.3 2007/03/16 00:09:43 r_sijrier Exp $
+    $Id: SplitClip.h,v 1.4 2007/04/25 21:23:40 r_sijrier Exp $
 */
 
 #ifndef SPLITCLIPACTION_H
@@ -30,23 +30,34 @@ class AudioClip;
 class Track;
 class SongView;
 class AudioClipView;
+class SplitCursor;
 
 class SplitClip : public Command
 {
 public :
-        SplitClip(AudioClipView* view);
+	SplitClip(AudioClipView* view);
         ~SplitClip() {};
 
         int prepare_actions();
         int do_action();
         int undo_action();
 
+	int begin_hold();
+	int finish_hold();
+	void cancel_action();
+	void set_cursor_shape(int useX, int useY);
+
+	int jog();
+	
 private :
 	SongView* m_sv;
+	AudioClipView* m_cv;
         Track* m_track;
         AudioClip* m_clip;
         AudioClip* leftClip;
         AudioClip* rightClip;
+	nframes_t m_splitPoint;
+	SplitCursor* m_splitcursor;
 };
 
 #endif
