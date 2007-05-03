3ca0454ef292ce2d0866de20fbca87b41f50922d
Remon Sijrier <remon@traverso-daw.org>
2007-05-03 18:15:32 +0000

    * include cfloat which defines FLT_EPSILON

--- src/core/Project.cpp
+++ src/core/Project.cpp
@@ -27,6 +27,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <stdlib.h>
+#include <cfloat>
 
 #include "Project.h"
 #include "Song.h"
