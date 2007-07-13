8217cd19754b3c287c79bae8da416e3a4186bdaf
Nicola Doebelin <ndoebelin@gmx.ch>
2007-07-13 17:15:50 +0000

    added FLAC support to deploy-osx.sh script

--- deploy-osx.sh
+++ deploy-osx.sh
@@ -20,13 +20,12 @@ cp /usr/local/lib/libsamplerate.0.dylib  traverso.app/Contents/Frameworks
 cp /usr/local/lib/libportaudio.2.dylib traverso.app/Contents/Frameworks
 cp /usr/local/lib/librdf.0.dylib traverso.app/Contents/Frameworks
 cp /usr/local/lib/librasqal.0.dylib traverso.app/Contents/Frameworks
-#cp /usr/local/lib/libglib-2.0.0.dylib traverso.app/Contents/Frameworks
-#cp /usr/local/lib/libintl.8.dylib traverso.app/Contents/Frameworks
 cp /usr/local/lib/libvorbisfile.3.dylib traverso.app/Contents/Frameworks
 cp /usr/local/lib/libvorbis.0.dylib traverso.app/Contents/Frameworks
 cp /usr/local/lib/libogg.0.dylib traverso.app/Contents/Frameworks
 cp /usr/local/lib/libmad.0.dylib traverso.app/Contents/Frameworks
-
+cp /usr/local/lib/libFLAC++.6.dylib traverso.app/Contents/Frameworks
+cp /usr/local/lib/libFLAC.8.dylib traverso.app/Contents/Frameworks
 cp /usr/local/lib/libraptor.1.dylib traverso.app/Contents/Frameworks
 cp /usr/local/lib/libst.0.dylib traverso.app/Contents/Frameworks
 cp $QT_PATH/lib/QtXml.framework/Versions/4/QtXml traverso.app/Contents/Frameworks/QtXml.framework/Versions/4/
@@ -39,12 +38,11 @@ install_name_tool -id @executable_path/../Frameworks/libsamplerate.0.dylib trave
 install_name_tool -id @executable_path/../Frameworks/libportaudio.2.dylib traverso.app/Contents/Frameworks/libportaudio.2.dylib
 install_name_tool -id @executable_path/../Frameworks/librdf.0.dylib traverso.app/Contents/Frameworks/librdf.0.dylib
 install_name_tool -id @executable_path/../Frameworks/librasqal.0.dylib traverso.app/Contents/Frameworks/librasqal.0.dylib
-#install_name_tool -id @executable_path/../Frameworks/libglib-2.0.0.dylib traverso.app/Contents/Frameworks/libglib-2.0.0.dylib
-#install_name_tool -id @executable_path/../Frameworks/libintl.8.dylib traverso.app/Contents/Frameworks/libintl.8.dylib
 install_name_tool -id @executable_path/../Frameworks/libvorbisfile.3.dylib traverso.app/Contents/Frameworks/libvorbisfile.3.dylib
 install_name_tool -id @executable_path/../Frameworks/libmad.0.dylib traverso.app/Contents/Frameworks/libmad.0.dylib
 install_name_tool -id @executable_path/../Frameworks/libraptor.1.dylib traverso.app/Contents/Frameworks/libraptor.1.dylib
 install_name_tool -id @executable_path/../Frameworks/libst.0.dylib traverso.app/Contents/Frameworks/libst.0.dylib
+install_name_tool -id @executable_path/../Frameworks/libFLAC++.6.dylib traverso.app/Contents/Frameworks/libFLAC++.6.dylib
 install_name_tool -id @executable_path/../Frameworks/QtXml.framework/Versions/4/QtXml traverso.app/Contents/Frameworks/QtXml.framework/Versions/4/QtXml
 install_name_tool -id @executable_path/../Frameworks/QtOpenGL.framework/Versions/4/QtOpenGL traverso.app/Contents/Frameworks/QtOpenGL.framework/Versions/4/QtOpenGL
 install_name_tool -id @executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui traverso.app/Contents/Frameworks/QtGui.framework/Versions/4/QtGui
@@ -55,11 +53,10 @@ install_name_tool -change /usr/local/lib/libsamplerate.0.dylib @executable_path/
 install_name_tool -change /usr/local/lib/libportaudio.2.dylib @executable_path/../Frameworks/libportaudio.2.dylib traverso.app/Contents/MacOS/traverso
 install_name_tool -change /usr/local/lib/librdf.0.dylib @executable_path/../Frameworks/librdf.0.dylib traverso.app/Contents/MacOS/traverso
 install_name_tool -change /usr/local/lib/librasqal.0.dylib @executable_path/../Frameworks/librasqal.0.dylib traverso.app/Contents/MacOS/traverso
-#install_name_tool -change /usr/local/lib/libglib-2.0.0.dylib @executable_path/../Frameworks/libglib-2.0.0.dylib traverso.app/Contents/MacOS/traverso
-#install_name_tool -change /usr/local/lib/libintl.8.dylib @executable_path/../Frameworks/libintl.8.dylib traverso.app/Contents/MacOS/traverso
 install_name_tool -change /usr/local/lib/libvorbisfile.3.dylib @executable_path/../Frameworks/libvorbisfile.3.dylib traverso.app/Contents/MacOS/traverso
 install_name_tool -change /usr/local/lib/libmad.0.dylib @executable_path/../Frameworks/libmad.0.dylib traverso.app/Contents/MacOS/traverso
 install_name_tool -change /usr/local/lib/libraptor.1.dylib @executable_path/../Frameworks/libraptor.1.dylib traverso.app/Contents/MacOS/traverso
+install_name_tool -change /usr/local/lib/libFLAC++.6.dylib @executable_path/../Frameworks/libFLAC++.6.dylib traverso.app/Contents/MacOS/traverso
 install_name_tool -change /usr/local/lib/libst.0.dylib @executable_path/../Frameworks/libst.0.dylib traverso.app/Contents/MacOS/sox
 install_name_tool -change /usr/local/lib/libsndfile.1.dylib @executable_path/../Frameworks/libsndfile.1.dylib traverso.app/Contents/MacOS/sox
 install_name_tool -change /usr/local/lib/libsamplerate.0.dylib @executable_path/../Frameworks/libsamplerate.0.dylib traverso.app/Contents/MacOS/sox
@@ -88,6 +85,12 @@ install_name_tool -change /usr/local/lib/libogg.0.dylib @executable_path/../Fram
 
 install_name_tool -change /usr/local/lib/libogg.0.dylib @executable_path/../Frameworks/libogg.0.dylib traverso.app/Contents/Frameworks/libogg.0.dylib
 install_name_tool -change /usr/local/lib/libmad.0.dylib @executable_path/../Frameworks/libmad.0.dylib traverso.app/Contents/Frameworks/libmad.0.dylib
+
+install_name_tool -change /usr/local/lib/libFLAC++.6.dylib @executable_path/../Frameworks/libFLAC++.6.dylib traverso.app/Contents/Frameworks/libFLAC++.6.dylib
+install_name_tool -change /usr/local/lib/libFLAC.8.dylib @executable_path/../Frameworks/libFLAC.8.dylib traverso.app/Contents/Frameworks/libFLAC++.6.dylib
+install_name_tool -change /usr/local/lib/libogg.0.dylib @executable_path/../Frameworks/libogg.0.dylib traverso.app/Contents/Frameworks/libFLAC++.6.dylib
+install_name_tool -change /usr/local/lib/libFLAC.8.dylib @executable_path/../Frameworks/libFLAC.8.dylib traverso.app/Contents/Frameworks/libFLAC.8.dylib
+install_name_tool -change /usr/local/lib/libogg.0.dylib @executable_path/../Frameworks/libogg.0.dylib traverso.app/Contents/Frameworks/libFLAC.8.dylib
 
 mv traverso.app Traverso.app
 
