7e704a51c290bbef6eb3c3c59dbd695600dc8f5b
Remon Sijrier <remon@traverso-daw.org>
2007-07-18 13:56:52 +0000

    try MadAudioReader as last option in create_audio_reader() since it's the most expensive one, and mp3 fileformat is not supported in the previous AudioReaders

--- src/core/AbstractAudioReader.cpp
+++ src/core/AbstractAudioReader.cpp
@@ -78,17 +78,17 @@ AbstractAudioReader* AbstractAudioReader::create_audio_reader(const QString& fil
 {
 	AbstractAudioReader* newReader;
 	
-	if (VorbisAudioReader::can_decode(filename)) {
-		newReader = new VorbisAudioReader(filename);
-	}
-	else if (MadAudioReader::can_decode(filename)) {
-		newReader = new MadAudioReader(filename);
-	}
-	else if (FlacAudioReader::can_decode(filename)) {
+	if (FlacAudioReader::can_decode(filename)) {
 		newReader = new FlacAudioReader(filename);
+	}
+	else if (VorbisAudioReader::can_decode(filename)) {
+		newReader = new VorbisAudioReader(filename);
 	}
 	else if (SFAudioReader::can_decode(filename)) {
 		newReader = new SFAudioReader(filename);
+	}
+	else if (MadAudioReader::can_decode(filename)) {
+		newReader = new MadAudioReader(filename);
 	}
 	else {
 		return 0;
