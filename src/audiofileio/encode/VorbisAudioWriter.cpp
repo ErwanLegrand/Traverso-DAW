b7211bfb2e286caca27405c954196fd0096664f0
Ben Levitt <levittben@yahoo.com>
2007-09-21 23:51:41 +0000

    Remove incorrect comment

--- src/audiofileio/encode/VorbisAudioWriter.cpp
+++ src/audiofileio/encode/VorbisAudioWriter.cpp
@@ -37,7 +37,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 #include "Debugger.h"
 
 
-// This just exists to keep lame types private and out of LameAudioWriter.h
 class VorbisAudioWriter::Private
 {
 public:
