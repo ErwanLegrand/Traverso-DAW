c59a27f76c78d050cdb14d77fdf548afd82659c7
Remon Sijrier <remon@traverso-daw.org>
2007-11-27 18:11:57 +0000

    * and also sort the trackviews itself before layouting them BUT do it without messing with the sortindex before doing 'it'

--- src/traverso/songcanvas/SongView.cpp
+++ src/traverso/songcanvas/SongView.cpp
@@ -199,14 +199,15 @@ void SongView::add_new_trackview(Track* track)
 {
 	TrackView* view = new TrackView(this, track);
 	
-	int sortIndex = track->get_sort_index();
+/*	int sortIndex = track->get_sort_index();
 	
 	if (sortIndex < 0) {
 		sortIndex = m_trackViews.size();
 		track->set_sort_index(sortIndex);
 	}
-	
+	*/
 	m_trackViews.append(view);
+	
 	if (m_trackViews.size() > 1) {
 		int height = m_trackViews.at(m_trackViews.size()-2)->get_track()->get_height();
 		m_trackViews.at(m_trackViews.size()-1)->get_track()->set_height(height);
