6962d163d355fef998b47cbf25bf90eee65b122d
Remon Sijrier <remon@traverso-daw.org>
2008-01-02 22:05:19 +0000

    * updated to work with autopackage . Just run make-autopackage.sh and all should be well (if your qmake is that of an apbuild one)

--- make-autopackage.sh
+++ make-autopackage.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 unset CXX2 # disable C++ ABI workaround since we're statically linking all C++ libs
-export CXX1=g++-gcc-3.4.6
+export CXX1=g++
 
 #slv2=$PWD/autopackage/static-libs/libslv2.a
 # libGLU needs to be statically linked since it's a C++ lib, the others are uncommon
-export APBUILD_STATIC="rdf=$PWD/autopackage/static-libs/librdf.a raptor=$PWD/autopackage/static-libs/libraptor.a rasqal=$PWD/autopackage/static-libs/librasqal.a GLU=$PWD/autopackage/static-libs/libGLU.a samplerate sqlite3"
+export APBUILD_DEBUG=1
+export APBUILD_STATIC="rdf=$PWD/autopackage/static-libs/librdf.a raptor=$PWD/autopackage/static-libs/libraptor.a rasqal=$PWD/autopackage/static-libs/librasqal.a wavpack=$PWD/autopackage/static-libs/libwavpack.a samplerate sqlite3 libXrender"
 
-export QTDIR=/usr/local/Trolltech/Qt-4.3.0-apbuild/
+export QTDIR=/usr/local/Trolltech/Qt-4.3.3
 makepackage
