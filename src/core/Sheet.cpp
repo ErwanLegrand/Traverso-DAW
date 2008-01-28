d6e710fddf53651c1f69fd396af5d7007b9d271c
Remon Sijrier <remon@traverso-daw.org>
2008-01-28 20:54:16 +0000

    * only allow zooming to 0.5 (== 2 pixels for 1 audio sample)

--- src/core/Sheet.cpp
+++ src/core/Sheet.cpp
@@ -748,8 +748,8 @@ void Sheet::set_hzoom( qreal hzoom )
 		hzoom = Peak::max_zoom_value();
 	}
 	
-	if (hzoom < 0.1) {
-		hzoom = 0.1;
+	if (hzoom < 0.5) {
+		hzoom = 0.5;
 	}
 	
 	if (m_hzoom == hzoom) {
