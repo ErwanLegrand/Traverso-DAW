e3691ae4f49f9b875bc5cd2eb8684167c7c48eb2
Remon Sijrier <remon@traverso-daw.org>
2009-03-07 16:54:27 +0000

    * correctly calculate the 'real' new edge position, so when the old pos == new pos do nothing!

--- src/commands/MoveEdge.cpp
+++ src/commands/MoveEdge.cpp
@@ -1,28 +1,30 @@
 /*
-    Copyright (C) 2005-2007 Remon Sijrier 
- 
-    This file is part of Traverso
- 
-    Traverso is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
- 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
- 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
- 
+Copyright (C) 2005-2009 Remon Sijrier 
+
+This file is part of Traverso
+
+Traverso is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
+
 */
 
-#include <libtraversocore.h>
-
 #include "MoveEdge.h"
-#include <ViewPort.h>
+
+#include "AudioClip.h"
+#include "ContextPointer.h"
+#include "Sheet.h"
+#include "SnapList.h"
 #include <SheetView.h>
 #include <AudioClipView.h>
 
@@ -35,10 +37,10 @@
 MoveEdge::MoveEdge(AudioClipView* cv, SheetView* sv, QByteArray whichEdge)
 	: Command(cv->get_clip(), tr("Move Clip Edge"))
 {
-        m_cv = cv;
-        m_sv = sv;
+	m_cv = cv;
+	m_sv = sv;
 	m_clip = cv->get_clip();
-        m_edge = whichEdge;
+	m_edge = whichEdge;
 }
 
 
@@ -47,32 +49,32 @@ MoveEdge::~MoveEdge()
 
 int MoveEdge::prepare_actions()
 {
-        PENTER;
+	PENTER;
 	
 	if (m_newPos == m_originalPos) {
 		// Nothing happened!
 		return -1;
 	}
-        
-        return 1;
+	
+	return 1;
 }
 
 int MoveEdge::begin_hold()
 {
 	PENTER;
-        if (m_edge == "set_left_edge") {
-                m_newPos = m_originalPos = m_clip->get_track_start_location();
+	if (m_edge == "set_left_edge") {
+		m_newPos = m_originalPos = m_clip->get_track_start_location();
 		m_otherEdgePos = m_clip->get_track_end_location();
 	}
-        if (m_edge == "set_right_edge") {
-                m_newPos = m_originalPos = m_clip->get_track_end_location();
+	if (m_edge == "set_right_edge") {
+		m_newPos = m_originalPos = m_clip->get_track_end_location();
 		m_otherEdgePos = m_clip->get_track_start_location();
 	}
 
 	m_clip->set_snappable(false);
 	m_sv->stop_follow_play_head();
 
-        return 1;
+	return 1;
 }
 
 
@@ -80,7 +82,7 @@ int MoveEdge::finish_hold()
 {
 	m_clip->set_snappable(true);
 
-        return 1;
+	return 1;
 }
 
 
@@ -128,7 +130,12 @@ int MoveEdge::jog()
 		m_newPos = m_otherEdgePos - (2 * m_sv->timeref_scalefactor);
 	}
 
-        return do_action();
+	// use this function to actually set the clip edge
+	do_action();
+	
+	// AudioClip does the calculation and corrects is we wanted to move the 
+	// edge to far. So set m_newPos to the _real_ track end location.
+	m_newPos = m_clip->get_track_end_location();
 }
 
 // eof
