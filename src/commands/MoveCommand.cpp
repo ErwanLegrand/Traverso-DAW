ea7d81d8cf967042f866c678fd895ec1c259ac11
Remon Sijrier <remon@traverso-daw.org>
2010-04-11 20:08:55 +0200

    move common move functionality in it's own base class

---
+++ src/commands/MoveCommand.cpp
@@ -0,0 +1,67 @@
+/*
+    Copyright (C) 2010 Remon Sijrier
+
+    This file is part of Traverso
+
+    Traverso is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
+
+*/
+
+#include "MoveCommand.h"
+
+
+MoveCommand::MoveCommand(const QString &description)
+        : Command(description)
+        , m_speed(1)
+{
+}
+
+MoveCommand::MoveCommand(ContextItem* item, const QString &description)
+        : Command(item, description)
+        , m_speed(1)
+{
+}
+
+void MoveCommand::move_faster(bool autorepeat)
+{
+        if (m_speed == 1) {
+                m_speed = 2;
+        } else if (m_speed == 2) {
+                m_speed = 4;
+        } else if (m_speed == 4) {
+                m_speed = 8;
+        } else if (m_speed == 8) {
+                m_speed = 16;
+        } else if (m_speed == 16) {
+                m_speed = 32;
+        }
+}
+
+
+void MoveCommand::move_slower(bool autorepeat)
+{
+        if (m_speed == 32) {
+                m_speed = 16;
+        } else if (m_speed == 16) {
+                m_speed = 8;
+        } else if (m_speed == 8) {
+                m_speed = 4;
+        } else if (m_speed == 4) {
+                m_speed = 2;
+        } else if (m_speed == 2) {
+                m_speed = 1;
+        }
+}
+
---
+++ src/commands/MoveCommand.h
@@ -0,0 +1,48 @@
+/*
+    Copyright (C) 2010 Remon Sijrier
+
+    This file is part of Traverso
+
+    Traverso is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
+
+*/
+
+#ifndef MOVE_COMMAND_H
+#define MOVE_COMMAND_H
+
+#include <Command.h>
+#include <defines.h>
+
+class MoveCommand : public Command
+{
+        Q_OBJECT
+        Q_CLASSINFO("move_faster", tr("Move Faster"));
+        Q_CLASSINFO("move_slower", tr("Move Slower"));
+
+public :
+        MoveCommand (const QString& description);
+        MoveCommand (ContextItem* item, const QString& description);
+        virtual ~MoveCommand (){};
+
+protected :
+        int             m_speed;
+
+public slots:
+        void move_faster(bool autorepeat);
+        void move_slower(bool autorepeat);
+
+};
+
+#endif // MOVECOMMAND_H
