c7bdc4a8ae60b46a6c87b9e4a7c3b2ecb7fe5c67
Remon Sijrier <remon@traverso-daw.org>
2010-04-26 15:00:04 +0200

    synced to latest version from Ardour

--- src/common/fpu.cc
+++ src/common/fpu.cc
@@ -29,86 +29,93 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 
 FPU::FPU ()
 {
-	unsigned long cpuflags = 0;
-
-	_flags = Flags (0);
+        _flags = Flags (0);
 
 #ifndef ARCH_X86
-	return;
-#endif
-	
-#ifndef USE_X86_64_ASM
-	asm volatile (
-		"mov $1, %%eax\n"
-		"pushl %%ebx\n"
-		"cpuid\n"
-		"movl %%edx, %0\n"
-		"popl %%ebx\n"
-		: "=r" (cpuflags)
-		: 
-		: "%eax", "%ecx", "%edx", "memory"
-		);
-	
+        return;
+
 #else
-	
-	asm volatile (
-		"pushq %%rbx\n"
-		"movq $1, %%rax\n"
-		"cpuid\n"
-		"movq %%rdx, %0\n"
-		"popq %%rbx\n"
-		: "=r" (cpuflags)
-		: 
-		: "%rax", "%rcx", "%rdx", "memory"
-		);
+
+        /* asm notes: although we explicitly save&restore ebx/rbx (stack pointer), we must tell
+           gcc that ebx,rbx is clobbered so that it doesn't try to use it as an intermediate
+           register when storing edx/rdx. gcc 4.3 didn't make this "mistake", but gcc 4.4
+           does, at least on x86_64.
+        */
+
+#ifndef USE_X86_64_ASM
+        asm volatile (
+                "mov $1, %%eax\n"
+                "pushl %%ebx\n"
+                "cpuid\n"
+                "movl %%edx, %0\n"
+                "popl %%ebx\n"
+                : "=r" (cpuflags)
+                :
+                : "%eax", "%ebx", "%ecx", "%edx"
+                );
+
+#else
+
+        asm volatile (
+                "pushq %%rbx\n"
+                "movq $1, %%rax\n"
+                "cpuid\n"
+                "movq %%rdx, %0\n"
+                "popq %%rbx\n"
+                : "=r" (cpuflags)
+                :
+                : "%rax", "%rbx", "%rcx", "%rdx"
+                );
 
 #endif /* USE_X86_64_ASM */
-	
-	if (cpuflags & (1<<25)) {
-		_flags = Flags (_flags | (HasSSE|HasFlushToZero));
-	}
 
-	if (cpuflags & (1<<26)) {
-		_flags = Flags (_flags | HasSSE2);
-	}
+        if (cpuflags & (1<<25)) {
+                _flags = Flags (_flags | (HasSSE|HasFlushToZero));
+        }
 
-	if (cpuflags & (1 << 24)) {
-		
-		char* fxbuf = 0;
-		char** temp = &fxbuf;
-		
+        if (cpuflags & (1<<26)) {
+                _flags = Flags (_flags | HasSSE2);
+        }
+
+        if (cpuflags & (1 << 24)) {
+
+                char* fxbuf = 0;
+
 #ifdef NO_POSIX_MEMALIGN
-		if ((fxbuf = (char *) malloc(512)) == 0)
+                if ((fxbuf = (char *) malloc(512)) == 0)
 #else
-		if (posix_memalign ((void**)temp, 16, 512))
-#endif			
-		{
-			printf("FPU() ERROR: cannot allocate 16 byte aligned buffer for h/w feature detection");
-		} else {
-			
-			asm volatile (
-				"fxsave (%0)"
-				:
-				: "r" (fxbuf)
-				: "memory"
-				);
-			
-			uint32_t mxcsr_mask = *((uint32_t*) &fxbuf[28]);
-			
-			/* if the mask is zero, set its default value (from intel specs) */
-			
-			if (mxcsr_mask == 0) {
-				mxcsr_mask = 0xffbf;
-			}
-			
-			if (mxcsr_mask & (1<<6)) {
-				_flags = Flags (_flags | HasDenormalsAreZero);
-			} 
+                if (posix_memalign ((void**)&fxbuf, 16, 512))
+#endif
+                {
+                        error << _("cannot allocate 16 byte aligned buffer for h/w feature detection") << endmsg;
+                } else {
 
-			free (fxbuf);
-		}
-	}
-}			
+                        memset (fxbuf, 0, 512);
+
+                        asm volatile (
+                                "fxsave (%0)"
+                                :
+                                : "r" (fxbuf)
+                                : "memory"
+                                );
+
+                        uint32_t mxcsr_mask = *((uint32_t*) &fxbuf[28]);
+
+                        /* if the mask is zero, set its default value (from intel specs) */
+
+                        if (mxcsr_mask == 0) {
+                                mxcsr_mask = 0xffbf;
+                        }
+
+                        if (mxcsr_mask & (1<<6)) {
+                                _flags = Flags (_flags | HasDenormalsAreZero);
+                        }
+
+                        free (fxbuf);
+                }
+        }
+#endif  // ARCH_X86
+}
 
 FPU::~FPU ()
 {
