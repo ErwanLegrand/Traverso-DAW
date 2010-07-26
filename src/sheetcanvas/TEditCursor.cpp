79fe458cbcea16907678786a1ae259488297b5b9
Remon Sijrier <remon@traverso-daw.org>
2010-07-26 22:38:43 +0200

    moved HoldCursor to its own file

---
+++ src/sheetcanvas/TEditCursor.cpp
@@ -0,0 +1,117 @@
+/*
+Copyright (C) 2010 Remon Sijrier
+
+This file is part of Traverso
+
+Traverso is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
+
+*/
+
+
+#include "TEditCursor.h"
+
+#include "ClipsViewPort.h"
+#include "SheetView.h"
+#include "ViewPort.h"
+
+TEditCursor::TEditCursor(SheetView* sv)
+        : m_sv(sv)
+{
+        m_textItem = new QGraphicsTextItem(this);
+        m_textItem->setFont(themer()->get_font("ViewPort:fontscale:infocursor"));
+
+        setZValue(200);
+}
+
+TEditCursor::~TEditCursor( )
+{
+}
+
+void TEditCursor::paint( QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget )
+{
+        Q_UNUSED(widget);
+        Q_UNUSED(option);
+
+        painter->drawPixmap(0, 0, m_pixmap);
+}
+
+
+void TEditCursor::set_text( const QString & text )
+{
+        m_text = text;
+
+        if (!m_text.isEmpty()) {
+                QString html = "<html><body bgcolor=ghostwhite>" + m_text + "</body></html>";
+                m_textItem->setHtml(html);
+                m_textItem->show();
+        } else {
+                m_textItem->hide();
+        }
+}
+
+void TEditCursor::set_type( const QString & type )
+{
+        QPointF origPos = scenePos();
+        origPos.setX(origPos.x() + (qreal(m_pixmap.width()) / 2));
+        origPos.setY(origPos.y() + (qreal(m_pixmap.height()) / 2));
+        m_pixmap = find_pixmap(type);
+        set_pos(origPos);
+}
+
+QRectF TEditCursor::boundingRect( ) const
+{
+        return QRectF(0, 0, m_pixmap.width(), m_pixmap.height());
+}
+
+void TEditCursor::reset()
+{
+        m_text = "";
+        m_textItem->hide();
+}
+
+void TEditCursor::set_pos(QPointF p)
+{
+        ViewPort* vp = m_sv->get_clips_viewport();
+        int x = vp->mapFromScene(pos()).x();
+        int y = vp->mapFromScene(pos()).y();
+        int yoffset = m_pixmap.height() + 25;
+
+        if (y < 0) {
+                yoffset = - y;
+        } else if (y > vp->height() - m_pixmap.height()) {
+                yoffset = vp->height() - y - m_pixmap.height();
+        }
+
+        int diff = vp->width() - (x + m_pixmap.width() + 8);
+
+        if (diff < m_textItem->boundingRect().width()) {
+                m_textItem->setPos(diff - m_pixmap.width(), yoffset);
+        } else if (x < -m_pixmap.width()) {
+                m_textItem->setPos(8 - x, yoffset);
+        } else {
+                m_textItem->setPos(m_pixmap.width() + 8, yoffset);
+        }
+
+        p.setX(p.x() - (qreal(m_pixmap.width()) / 2));
+        p.setY(p.y() - (qreal(m_pixmap.height()) / 2));
+
+        setPos(p);
+}
+
+QPointF TEditCursor::get_scene_pos()
+{
+        QPointF shapeAdjust(boundingRect().width() / 2, boundingRect().height() / 2);
+        return scenePos() + shapeAdjust;
+}
---
+++ src/sheetcanvas/TEditCursor.h
@@ -0,0 +1,56 @@
+/*
+Copyright (C) 2010 Remon Sijrier
+
+This file is part of Traverso
+
+Traverso is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
+
+*/
+
+#ifndef TEDITCURSOR_H
+#define TEDITCURSOR_H
+
+#include "ViewItem.h"
+
+class SheetView;
+
+class TEditCursor : public ViewItem
+{
+        Q_OBJECT
+public:
+        TEditCursor(SheetView* sv);
+        ~TEditCursor();
+
+        void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+
+        void set_text(const QString& text);
+        void set_type(const QString& type);
+        void set_pos(QPointF pos);
+
+        // returns the real cursor position, i.e. the center of the cursor.
+        QPointF get_scene_pos();
+        void reset();
+
+        QRectF boundingRect() const;
+
+private:
+        SheetView* 	m_sv;
+        QGraphicsTextItem* m_textItem;
+        QPoint          m_pos;
+        QPixmap		m_pixmap;
+        QString         m_text;
+};
+
+#endif // TEDITCURSOR_H
