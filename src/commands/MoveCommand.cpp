66299a21418664a711369238a5e0188291ce21f7
Remon Sijrier <remon@traverso-daw.org>
2011-03-21 10:58:28 +0100

    use snap state from song
    instead of setting snap state for each move command

--- src/commands/MoveCommand.cpp
+++ src/commands/MoveCommand.cpp
@@ -24,112 +24,118 @@
 #include "InputEngine.h"
 #include "Project.h"
 #include "ProjectManager.h"
+#include "Sheet.h"
 
 #include "Debugger.h"
 
 MoveCommand::MoveCommand(const QString &description)
-        : TCommand(description)
-        , m_doSnap(false)
+	: TCommand(description)
 {
-        QString collected = ie().get_collected_number();
-        if (!(collected.isEmpty() || collected.isNull())) {
-                set_collected_number(collected);
-        } else {
-                m_speed = pm().get_project()->get_keyboard_arrow_key_navigation_speed();
-        }
+	QString collected = ie().get_collected_number();
+	if (!(collected.isEmpty() || collected.isNull())) {
+		set_collected_number(collected);
+	} else {
+		m_speed = pm().get_project()->get_keyboard_arrow_key_navigation_speed();
+	}
+
+	Sheet* sheet = pm().get_project()->get_active_sheet();
+	m_doSnap = sheet->is_snap_on();
 }
 
 MoveCommand::MoveCommand(ContextItem* item, const QString &description)
-        : TCommand(item, description)
-        , m_doSnap(false)
+	: TCommand(item, description)
+	, m_doSnap(false)
 {
-        QString collected = ie().get_collected_number();
-        if (collected.size()) {
-                set_collected_number(collected);
-        } else {
-                m_speed = pm().get_project()->get_keyboard_arrow_key_navigation_speed();
-        }
+	QString collected = ie().get_collected_number();
+	if (collected.size()) {
+		set_collected_number(collected);
+	} else {
+		m_speed = pm().get_project()->get_keyboard_arrow_key_navigation_speed();
+	}
 }
 
 void MoveCommand::move_faster(bool autorepeat)
 {
-        if (m_speed > 32) {
-                m_speed = 32;
-        }
+	if (m_speed > 32) {
+		m_speed = 32;
+	}
 
-        if (m_speed == 1) {
-                m_speed = 2;
-        } else if (m_speed == 2) {
-                m_speed = 4;
-        } else if (m_speed == 4) {
-                m_speed = 8;
-        } else if (m_speed == 8) {
-                m_speed = 16;
-        } else if (m_speed == 16) {
-                m_speed = 32;
-        }
+	if (m_speed == 1) {
+		m_speed = 2;
+	} else if (m_speed == 2) {
+		m_speed = 4;
+	} else if (m_speed == 4) {
+		m_speed = 8;
+	} else if (m_speed == 8) {
+		m_speed = 16;
+	} else if (m_speed == 16) {
+		m_speed = 32;
+	}
 
-        pm().get_project()->set_keyboard_arrow_key_navigation_speed(m_speed);
+	pm().get_project()->set_keyboard_arrow_key_navigation_speed(m_speed);
 }
 
 
 void MoveCommand::move_slower(bool autorepeat)
 {
-        if (m_speed > 32) {
-                m_speed = 32;
-        }
+	if (m_speed > 32) {
+		m_speed = 32;
+	}
 
-        if (m_speed == 32) {
-                m_speed = 16;
-        } else if (m_speed == 16) {
-                m_speed = 8;
-        } else if (m_speed == 8) {
-                m_speed = 4;
-        } else if (m_speed == 4) {
-                m_speed = 2;
-        } else if (m_speed == 2) {
-                m_speed = 1;
-        }
+	if (m_speed == 32) {
+		m_speed = 16;
+	} else if (m_speed == 16) {
+		m_speed = 8;
+	} else if (m_speed == 8) {
+		m_speed = 4;
+	} else if (m_speed == 4) {
+		m_speed = 2;
+	} else if (m_speed == 2) {
+		m_speed = 1;
+	}
 
-        pm().get_project()->set_keyboard_arrow_key_navigation_speed(m_speed);
+	pm().get_project()->set_keyboard_arrow_key_navigation_speed(m_speed);
 }
 
 void MoveCommand::set_collected_number(const QString &collected)
 {
-        PENTER;
-        int number = 0;
-        bool ok = false;
-        QString cleared = collected;
-        cleared = cleared.remove(".").remove("-").remove(",");
+	PENTER;
+	int number = 0;
+	bool ok = false;
+	QString cleared = collected;
+	cleared = cleared.remove(".").remove("-").remove(",");
 
-        if (cleared.size() >= 1) {
-                number = QString(cleared.data()[cleared.size() -1]).toInt(&ok);
-        }
+	if (cleared.size() >= 1) {
+		number = QString(cleared.data()[cleared.size() -1]).toInt(&ok);
+	}
 
-        if (ok) {
-                switch(number) {
-                case 0: m_speed = 1; break;
-                case 1: m_speed = 2; break;
-                case 2: m_speed = 4; break;
-                case 3: m_speed = 8; break;
-                case 4: m_speed = 16; break;
-                case 5: m_speed = 32; break;
-                case 6: m_speed = 64; break;
-                case 7: m_speed = 128; break;
-                case 8: m_speed = 128; break;
-                case 9: m_speed = 128; break;
-                default: m_speed = 2;
-                }
-        }
+	if (ok) {
+		switch(number) {
+		case 0: m_speed = 1; break;
+		case 1: m_speed = 2; break;
+		case 2: m_speed = 4; break;
+		case 3: m_speed = 8; break;
+		case 4: m_speed = 16; break;
+		case 5: m_speed = 32; break;
+		case 6: m_speed = 64; break;
+		case 7: m_speed = 128; break;
+		case 8: m_speed = 128; break;
+		case 9: m_speed = 128; break;
+		default: m_speed = 2;
+		}
+	}
 
-        pm().get_project()->set_keyboard_arrow_key_navigation_speed(m_speed);
+	pm().get_project()->set_keyboard_arrow_key_navigation_speed(m_speed);
 }
 
 
 void MoveCommand::toggle_snap_on_off(bool autorepeat)
 {
-        if (autorepeat) {
-                return;
-        }
-        m_doSnap = ! m_doSnap;
+	if (autorepeat) {
+		return;
+	}
+
+	Sheet* sheet = pm().get_project()->get_active_sheet();
+	sheet->toggle_snap();
+	m_doSnap = sheet->is_snap_on();
 }
