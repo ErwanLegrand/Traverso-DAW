f994792c37c734daecf2e2262d5bb3da4d83e507
Remon Sijrier <remon@traverso-daw.org>
2011-04-20 11:01:55 +0200

    set text color explicitly

--- src/sheetcanvas/PositionIndicator.cpp
+++ src/sheetcanvas/PositionIndicator.cpp
@@ -30,7 +30,7 @@ PositionIndicator::PositionIndicator(ViewItem* parentView)
 	: ViewItem(parentView, 0)
 {
 	calculate_bounding_rect();
-	setZValue(100);
+	setZValue(200);
 }
 
 void PositionIndicator::paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget)
@@ -39,6 +39,7 @@ void PositionIndicator::paint(QPainter * painter, const QStyleOptionGraphicsItem
 	Q_UNUSED(widget);
 	
 	painter->drawPixmap(0, 0, m_background);
+	painter->setPen(Qt::black);
         painter->drawText(m_boundingRect, Qt::AlignHCenter, m_value);
 }
 
@@ -52,7 +53,7 @@ void PositionIndicator::calculate_bounding_rect()
 	
 	QPainter painter(&m_background);
 	painter.setRenderHint(QPainter::Antialiasing);
-	painter.setBrush(QColor(255, 255, 255, 150));
+	painter.setBrush(QColor(255, 255, 255, 180));
 	painter.setPen(Qt::NoPen);
 	int rounding = m_boundingRect.height() / 2;
 	painter.drawRoundedRect(m_boundingRect, rounding, rounding);
